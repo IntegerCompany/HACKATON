529e6f26537195d873da2177922ab366d2165483
Ostap Ferneza <ostapfer@gmail.com>
2015-08-08 18:53:26 +0300

    single.page update

--- wp-content/themes/arcade-basic/content.php
+++ wp-content/themes/arcade-basic/content.php
@@ -15,7 +15,13 @@
 		get_template_part( 'content', 'header' ); ?>
 
 	    <div class="entry-content description clearfix">
-
+			<h1 class="entry-title taggedlink">
+				<?php if ( is_single() ) : ?>
+					<?php the_title(); ?>
+				<?php else : ?>
+					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+				<?php endif; // is_single() ?>
+			</h1>
 			<?php
 			if ( is_singular() && ! is_front_page() ) {
 				the_content(__('Read more', 'arcade'));
