f034236f3d9d474b086fabfe82f9dc21d3c84a42
KryvunRoman <kryvun.roman@gmail.com>
2015-08-14 11:56:44 +0300

    add and fixed blog page

--- wp-content/themes/arcade-basic/page-blog.php
+++ wp-content/themes/arcade-basic/page-blog.php
@@ -12,27 +12,22 @@ get_header();
     <div class="row">
         <h1><?php the_title(); ?></h1>
 
-        <div class="breadcrumb"></div>
+        <div class="breadcrumb">Home -> Blog  //TODO</div>
 
         <div class="blog_grid clearfix">
     <?php
         $blogs = load_blogs();
-//    echo "<pre>";
-//    print_r($blogs);
-//    echo "</pre>";
 ?>
             <ul>
             <?php
+
+            $date_format = 'jS \of F Y';
+
     foreach ($blogs as $blog)
     {
-        $date = explode(' ', $blog->post_date);
-        $date = explode('-', $date[0]);
-//        $author_post_nam = get_the_author( 'user_firstname',(int)$blog->post_author );
-//
-//        $author_post_lName = get_the_author( 'user_lastname', (int)$blog->post_author );
-//
-//        echo $blog->post_author;
-//        print_r(get_the_author( (int)$blog->post_author ));
+
+        $date =  $blog->post_date;
+        $date_published = new DateTime($date);
         ?>
             <li class="col-md-4 blog-element" >
                 <a href="<?php echo get_permalink($blog->ID); ?>">
@@ -41,7 +36,7 @@ get_header();
                 </div>
                 <div class="title_block clearfix">
                     <h3><?php echo $blog->post_title; ?></h3>
-                    <div class="publ_date"><i class="fa fa-calendar"></i>  <?php echo date('jS \of F Y', mktime(0,0,0,$date[2], $date[1], $date[0])); ?></div>
+                    <div class="publ_date"><i class="fa fa-calendar"></i>  <?php echo $date_published->format( $date_format ); ?></div>
                     <div class="publ_author"><?php echo __('by', TEXTDOMAIN).' '.get_the_author_meta('user_nicename', (int)$blog->post_author);//.$author_post_nam.' '.$author_post_lName; ?></div>
                 </div>
                 </a>
@@ -61,9 +56,10 @@ get_header();
         ;(function($) {
             $(document).ready(function(){
 
-                var is_loading = false;
-                var offset = 12;
-                var no_data = false;
+                var is_loading = false,
+                    offset = 12,
+                    no_data = false,
+                    blogLength = '<?php echo count($blogs); ?>';
 
                 function find_element()
                 {
@@ -73,25 +69,9 @@ get_header();
 
                 find_element();
 
-//                $('.more_blogs').bind('click', function(){
-//                    if ($(this).attr('data-attr') == "enabled")
-//                    {
-//                        $.ajax({
-//                            type: "POST",
-//                            url: '<?// echo get_site_url(); ?>///wp-admin/admin-ajax.php',
-//                            data: data,
-//                            success: function (data) {
-//                                console.log(data);
-//
-//                            }
-//
-//                        });
-//                    }
-//                });
-
 
                 $(document).scroll(function() {
-                    if (is_loading == false && no_data == false)
+                    if (is_loading == false && no_data == false && blogLength > offset-1 )
                     {
                         if ( ($(window).scrollTop() + $(window).height()) >  $('.load_here').offset().top  )
                         {
