feaddcae200603531c557f90faf54a98a983c0ff
Liubomyr <liubomyr.matveychuk@gmail.com>
2015-08-17 18:00:43 +0300

    edit plugin staff-page

--- wp-content/plugins/wp-staff-page/js/script.js
+++ wp-content/plugins/wp-staff-page/js/script.js
@@ -1,7 +1,5 @@
 (function($) {
    $(document).on('ready',function(){
-       console.log('s = ' + $('.photo-slider').find('div').lenght );
-       //if($('.photo-slider').children('div').lenght > 2) {
        if(true) {
            $('.photo-slider').slick({
                infinite: true,
@@ -14,25 +12,19 @@
                    {
                        breakpoint: 1024,
                        settings: {
-                           slidesToShow: 3,
-                           slidesToScroll: 3,
+                           slidesToShow: 2,
+                           slidesToScroll: 2,
                            infinite: true,
                        }
                    },
                    {
                        breakpoint: 600,
                        settings: {
-                           slidesToShow: 2,
-                           slidesToScroll: 2
-                       }
-                   },
-                   {
-                       breakpoint: 480,
-                       settings: {
                            slidesToShow: 1,
                            slidesToScroll: 1
                        }
                    }
+
 
                ]
            });
