0395fbc8e5feff59496e2fb219816e853a91f2c0
Bogdan <koiliable@gmail.com>
2015-08-21 12:30:24 +0300

    Change Image-Link (Webgrid) to proper dynamic links

--- wp-content/plugins/webgrid/get-image-link.php
+++ wp-content/plugins/webgrid/get-image-link.php
@@ -11,12 +11,14 @@
 
 	$postid = get_option('thumb-text');
 	$currentPost = get_post($postid[$_POST['count']]);
+
+    $dir = wp_upload_dir();
 ?>
 
 <img
-	src="http://integer.webdecision.com.ua/wp-content/uploads/greed/<?php echo get_option('thumb-'.$_POST['count']); ?>"
+	src="<?php echo $dir['baseurl']; ?>/wp-content/uploads/greed/<?php echo get_option('thumb-'.$_POST['count']); ?>"
 	data-page="<?php echo $currentPost->post_name; ?>"
 />
-<span class="dark">
+<div class="dark">
 	<h3><?php echo $currentPost->post_title; ?></h3>
-</span>
+</div>
