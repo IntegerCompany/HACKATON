c5168311ce6c846d872feebbf0fc8a718c07b774
KryvunRoman <kryvun.roman@gmail.com>
2015-08-27 16:44:46 +0300

    Fixed swipe in mob menu

--- wp-content/themes/arcade-basic/footer.php
+++ wp-content/themes/arcade-basic/footer.php
@@ -57,7 +57,7 @@
 
 <?php wp_footer(); ?>
 <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/function.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/function.min.js"></script>
 
 </body>
 </html>
--- wp-content/themes/arcade-basic/js/function.js
+++ wp-content/themes/arcade-basic/js/function.js
@@ -1,38 +1,38 @@
 
 	(function($){
-		//var $colapse = $(".collapse"),
-		//	$colapseMenu = $('.collapse > .logo-head > ul'),
-		//	leftPos,
-		//	DELAY = 500;
-        //
-		//function openMenu(leftPos){
-		//	$colapse.toggleClass('open');
-		//	leftPos = $colapse.hasClass('open') ? 0 : '-100%';
-		//	$colapseMenu.animate({'left': leftPos}, DELAY);
-		//}
-        //
-		//function swipeOpenMenu(leftPos){
-		//	leftPos == 0 ? $colapse.addClass('open') : $colapse.removeClass('open');
-		//	$colapseMenu.animate({'left': leftPos}, DELAY);
-		//}
-        //
-		//$("#button-mobile").on("click", openMenu );
-		//$(document).on( "swipeleft", function(){ swipeOpenMenu('-100%'); });
-		//$(document).on( "swiperight", function(){ swipeOpenMenu(0); });
-        //
-		//function swipeHandler( leftPos ){
-		//	$colapse.toggleClass('open');
-		//	$colapseMenu.animate({'left': leftPos}, DELAY);
-		//}
-        //
-		//$(".navbar-toggle").on("click", function(){
-		//	$colapse.is(":hidden") ? $colapse.slideDown("slow") : $colapse.slideUp("slow");
-		//});
-        //
-		//$(".logo-head a").click(function(){
-		//	//$('.navbar-toggle').is(':hidden') ? $(".logo-head > ul").css("left", "-100%") : $colapse.hide();
-		//	//$colapse.removeClass('open in');
-		//});
+		var $colapse = $(".collapse"),
+			$colapseMenu = $('.collapse > .logo-head > ul'),
+			leftPos,
+			DELAY = 500;
+
+		function openMenu(leftPos){
+			$colapse.toggleClass('open');
+			leftPos = $colapse.hasClass('open') ? 0 : '-100%';
+			$colapseMenu.animate({'left': leftPos}, DELAY);
+		}
+
+		function swipeOpenMenu(leftPos){
+			leftPos == 0 ? $colapse.addClass('open') : $colapse.removeClass('open');
+			$colapseMenu.animate({'left': leftPos}, DELAY);
+		}
+
+		$("#button-mobile").on("click", openMenu );
+		$(document).on( "swipeleft", function(){ swipeOpenMenu('-100%'); });
+		$(document).on( "swiperight", function(){ swipeOpenMenu(0); });
+
+		function swipeHandler( leftPos ){
+			$colapse.toggleClass('open');
+			$colapseMenu.animate({'left': leftPos}, DELAY);
+		}
+
+		$(".navbar-toggle").on("click", function(){
+			$colapse.is(":hidden") ? $colapse.slideDown("slow") : $colapse.slideUp("slow");
+		});
+
+		$(".logo-head a").click(function(){
+			$('.navbar-toggle').is(':hidden') ? $(".logo-head > ul").css("left", "-100%") : $colapse.hide();
+			$colapse.removeClass('open in');
+		});
 
 		$(document).ready(function(){
 			var topNavClass = "top-nav-collapse",
